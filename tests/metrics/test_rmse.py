e2c7ae1f982871dfcf59919347f0ae929b2acf4a
peterdudfield <peter.dudfield@hotmail.com>
2023-05-24 17:15:46 +0100

    fix rmse tests

--- tests/metrics/test_rmse.py
+++ tests/metrics/test_rmse.py
@@ -2,7 +2,7 @@ from nowcasting_metrics.metrics.rmse import make_rmse, make_rmse_all_gsp, make_r
 
 
 def test_make_rmse(db_session, gsp_yields, forecast_values_latest, datetime_interval):
-    value, n = make_rmse_one_gsp(session=db_session, datetime_interval=datetime_interval, gsp_id=1)
+    value, n = make_rmse_one_gsp(session=db_session, datetime_interval=datetime_interval, gsp_id=1, model_name="cnn")
 
     assert value == 4.5**0.5  # ((1-1)*0.5 + (4-1)**2*0.5)^0.5 = 4.5^0.5
     assert n == 2
@@ -20,7 +20,7 @@ def test_make_rmse_on_gsp(
 
 
 def test_make_rmse_all_gsp(db_session, gsp_yields, forecast_values_latest, datetime_interval):
-    value, n = make_rmse_all_gsp(session=db_session, datetime_interval=datetime_interval)
+    value, n = make_rmse_all_gsp(session=db_session, datetime_interval=datetime_interval, model_name="cnn")
 
     assert value == 4.5**0.5  # ((1-1)*0.5 + (4-1)**2*0.5)^0.5 = 4.5^0.5
     assert n == 10
